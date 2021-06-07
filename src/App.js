3e4dcd2e16fb5121bc8c61dd649ddb4a87b8a8c9
dwaynediddy <burrows.dwayne@icloud.com>
2021-06-07 12:12:08 +0200

    adding board and cards to be moveable. working but spacing is wrong

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { ThemeProvider } from 'styled-components'
 import { lightTheme, darkTheme } from './constants/theme'
 import { GlobalStyles } from './constants/global'
 
+import Board from './components/Board'
+import Card from './components/Card'
+
 function App() {
   const [theme, setTheme] = useState('light')
   const [text, setText] = useState('Light mode')
@@ -21,13 +24,31 @@ function App() {
 
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <>
+          <button onClick={toggleTheme}>{text}</button>
+          <h1>Dwaynes Trello Board</h1>
+          <main className="trello">
+        <Board id="Board-1" className="board">
+            <Card id="card-1" className="card" draggable="true">
+              <p>Card one</p>
+            </Card>
+          </Board>
+
+          <Board id="Board-2" className="board">
+            <Card id="card-2" className="card" draggable="true">
+              <p>Card two</p>
+            </Card>
+          </Board>
+
+          <Board id="Board-3" className="board">
+            <Card id="card-3" className="card" draggable="true">
+              <p>Card three</p>
+            </Card>
+          </Board>
       <GlobalStyles />
-      <button onClick={toggleTheme}>{text}</button>
-      <h1>Dwaynes Trello Board</h1>
-      </>
+      </main>
     </ThemeProvider>
-  );
+
+  )
 }
 
 export default App;
